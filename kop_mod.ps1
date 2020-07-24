7bf98b5fb7c1b2cef08ff12b016eed5c29b9b209
Patryk Zachnik <patrykz.pz@gmail.com>
2020-07-24 22:17:16 +0200

    Update kop_mod.ps1

--- kop_mod.ps1
+++ kop_mod.ps1
@@ -7,3 +7,23 @@ Copy-Item -Path "!Workshop/@ace/" -Destination "3cbgra/@ace" -Recurse -Container
 Copy-Item -Path "!Workshop/@ACE Compat - RHS United States Armed Forces/" -Destination "3cbgra/@ace_compat_rhs_usf3" -Recurse -Container: $false
 Copy-Item -Path "!Workshop/@ACE Compat - RHS- GREF/" -Destination "3cbgra/@ace_compat_rhs_gref3" -Recurse -Container: $false
 Copy-Item -Path "!Workshop/@ACE Compat - RHS Armed Forces of the Russian Federation/" -Destination "3cbgra/@ace_compat_rhs_afrf3" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@ACEX/" -Destination "3cbgra/@acex" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@Advanced Rappelling/" -Destination "3cbgra/@advanced_rappelling" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@Advanced Sling Loading/" -Destination "3cbgra/@advanced_sling_loading" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@Advanced Towing/" -Destination "3cbgra/@advanced_towing" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@Advanced Urban Rappelling/" -Destination "3cbgra/@advanced_urban_rappelling" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@Anizay/" -Destination "3cbgra/@anizay" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@ArmaZeusCache/" -Destination "3cbgra/@ArmaZeusCache" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@BackpackOnChest/" -Destination "3cbgra/@BackpackOnChest" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@CBA_A3/" -Destination "3cbgra/@cba_a3" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@CUP Terrains - Core/" -Destination "3cbgra/@cup_terrains_core" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@CUP Terrains - Maps/" -Destination "3cbgra/@cup_terrains_maps" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@Kujari" -Destination "3cbgra/@kujari" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@Project OPFOR/" -Destination "3cbgra/@ProjectOPFOR" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@RHSAFRF/" -Destination "3cbgra/@rhsafrf" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@RHSGREF/" -Destination "3cbgra/@rhsgref" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@RHSSAF/" -Destination "3cbgra/@rhssaf" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@RHSUSAF/" -Destination "3cbgra/@rhsusaf" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@Rosche, Germany/" -Destination "3cbgra/@rosche" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@task_force_radio/" -Destination "3cbgra/@TFAR" -Recurse -Container: $false
+Copy-Item -Path "!Workshop/@VET_Unflipping/" -Destination "3cbgra/@vet_unflipping" -Recurse -Container: $false
